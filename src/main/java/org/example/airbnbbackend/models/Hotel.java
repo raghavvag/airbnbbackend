349c107c745e21ab94cf135f3bb2bc20425ebd48
Raghav Agrawal <raghav.github69@gmail.com>
2025-09-08 21:55:56 +0530

    created hotel entity

---
+++ src/main/java/org/example/airbnbbackend/models/Hotel.java
@@ -0,0 +1,43 @@
+package org.example.airbnbbackend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String city;
+
+    @ElementCollection
+    @CollectionTable(name = "hotel_photos", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(name = "photo", columnDefinition = "TEXT")
+    private List<String> photos;
+
+    @ElementCollection
+    @CollectionTable(name = "hotel_amenities", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(name = "amenity", columnDefinition = "TEXT")
+    private List<String> amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}

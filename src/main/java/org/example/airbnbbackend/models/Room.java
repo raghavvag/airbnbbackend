2328b561f8d3d5f408bb44e53177645ed1f4a3bf
Raghav Agrawal <raghav.github69@gmail.com>
2025-09-26 21:19:54 +0530

    created room entity with all required attributes

---
+++ src/main/java/org/example/airbnbbackend/models/Room.java
@@ -0,0 +1,53 @@
+package org.example.airbnbbackend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+    @Column(nullable = false)
+    private String type;
+    @Column(nullable = false,precision = 10,scale = 2)
+    private BigDecimal basePrice;
+    @ElementCollection
+    @CollectionTable(name = "hotel_photos", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(name = "photo", columnDefinition = "TEXT")
+    private List<String> photos;
+
+    @ElementCollection
+    @CollectionTable(name = "hotel_amenities", joinColumns = @JoinColumn(name = "hotel_id"))
+    @Column(name = "amenity", columnDefinition = "TEXT")
+    private List<String> amenities;
+
+    @Column(nullable = false)
+    private Integer capacity;
+    @Column(nullable = false)
+    private Integer totalCount;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+}
